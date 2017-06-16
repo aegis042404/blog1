8a6d8d9880a32fe24b490f030f1f32491c13d9db
aegis042404 <aegis042404@gmail.com>
2017-06-16 03:14:54 -0700

    first template running

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post #import each model from models.py (ex.01) 
 
-# Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')#this is a query set, it filters data out of the database and choses the criteria to sort it ex.02
+
+    return render(request, 'blog/post_list.html', {'posts': posts})#ex03
